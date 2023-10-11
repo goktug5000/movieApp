9bbf2378f7f216d23b196f8c95d92de1bbe2bee6
goktug5000 <goktug5000@gmail.com>
2023-10-11 15:17:14 +0300

    obj kurdu

---
+++ src/app/movies/movies.components.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'movies', // <movies></movies>
+//  selector: '.movies' // <div class="movies"></div>
+//  selector: '#movies' // <div id="movies"></div>
+    templateUrl: 'movies.component.html'
+})
+export class MoviesComponent{
+
+}
