7eb87b1c698feeaa68b86da158ceb7f0fb6ee4a9
goktug5000 <goktug5000@gmail.com>
2023-10-12 14:01:37 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MoviesComponent } from './movies/movies.components';
+import { MoviesComponent } from './movies/movies.component';
+
 
 @NgModule({
   declarations: [
