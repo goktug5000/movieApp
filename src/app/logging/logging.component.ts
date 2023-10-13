adf9955b945c8652169326fcbd40956bd2c38a3d
goktug5000 <goktug5000@gmail.com>
2023-10-13 10:52:25 +0300

    Logging

--- src/app/logging/logging.component.ts
+++ src/app/logging/logging.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
-  selector: 'app-logging',
+  selector: 'logging',
   templateUrl: './logging.component.html',
   styleUrls: ['./logging.component.css']
 })
 export class LoggingComponent {
 
+
+  constructor(public loggingService:LoggingService){
+    
+  }
 }
--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -2,16 +2,21 @@ import { Injectable } from '@angular/core';
 import { Movie } from './movie';
 import { Movies } from './movie.datasource';
 import { Observable, of } from 'rxjs';
+import { LoggingService } from './logging.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(private loggingService:LoggingService) { 
+
+
+  }
 
 
   getMovies(): Observable<Movie[]>{
+    this.loggingService.add('movie.service.ts: listing movies');
     return of(Movies);
   }
 
